65bdbeb11f0c23567b4237c7aa6484a39c235058
Alex Warren <alex@alex-VPCSE13FX.(none)>
2013-06-29 16:33:27 -0700

    added ignore files and restructed directory

---
+++ matlab/cut_dead_time_alex.m
@@ -0,0 +1,11 @@
+speed_up = 25
+[whole_podcast, framerate] = audioread('podcast.mp3');
+podcast = whole_podcast(1:framerate*30,1);
+smoothed_podcast = smooth(abs(podcast),floor(framerate));
+%quantiles = quantile(abs(smoothed_podcast(smoothed_podcast > 0)),.01);
+range = range(smoothed_podcast)
+%podcast_under_threshold = abs(podcast) < quantiles;
+%smoothed_podcast_under_threshold = abs(smoothed_podcast) < quantiles;
+%live_time_frames = find(smoothed_podcast_under_threshold == 0);
+%audiowrite('chopped_podcast.mp4',whole_podcast(live_time_frames,:),framerate)
+
